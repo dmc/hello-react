3d43d49674337f4a9c520d2ef348225e81937575
dmc <dmc.system.service@gmail.com>
2022-12-14 11:19:30 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ReactComponent as ReactLogo } from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    // uncomment here to check eslint
+    // let major;
+    // let minor;
+    // const add = (x, y) => {
+    //   return x + y;
+    // };
+  }
+
+  handleClick() {
+    const banner = document.getElementById("banner-message");
+    banner.classList.contains("alt")
+      ? banner.classList.remove("alt")
+      : banner.classList.add("alt");
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="banner-message">
+          <p>Hello React</p>
+          <p>
+            <ReactLogo />
+          </p>
+          <button onClick={this.handleClick}>transform</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
